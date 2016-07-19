5adbb83a0f189cefb733caf30defe6c369a29fab
Administrator <Administrator@BSPARK-DT.mobile.asu.edu>
2016-07-19 17:03:41 +0900

    # WARNING: head commit changed in the meantime
    1st

--- src/Main.py
+++ src/Main.py
@@ -1,12 +1,19 @@
+#-*- coding: utf-8 -*-
 '''
 Created on Jul 16, 2016
 
 @author: Administrator
 '''
+
 import gscholar.gscholar as gscholar
 from zs.bibtex.parser import parse_string
+import FileIO.Excel as excel
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
-def GgscholarParsing(reference):
+
+def gscholarParsing(reference):
 #    output of gscholar.py
 #    dt - List
     gscResult = gscholar.query(reference)
@@ -16,18 +23,39 @@ def GgscholarParsing(reference):
 #    parsing one dictionary of bibliography
     for key, value in bibliography.items():
         article = value
-    
+ 
     return article
+#    return dt: zs.bibtex.structures.Article
+
+def articleToDictionary(article):
+    dic = {}
+    for keyUni, valueUni in article.items():
+        key = str(keyUni)
+        if type(valueUni) == list:
+            value = []
+            for elementUni in valueUni:
+                element = str(elementUni)
+                value.append(element)
+        else:
+            value = str(valueUni)
+        dic[key] = value
+    
+    return dic
+#    return dt: dictionary of string
 
 if __name__ == '__main__':
+    
     ### Reading references from csv file
+    refers = excel.excelRead("Book1.xlsx", "Book1")
+    
+    referList = []
+    for refer in refers:
+        referStr = str(refer[0])
+        referList.append(referStr)
     
     
     ### Parsing the references
+    article2 = gscholarParsing("Modeling spatial dimensions of housing prices in Milwaukee, WI")
+    dic2 = articleToDictionary(article2)
     
-    article1 = GgscholarParsing("Geographically weighted regression: the analysis of spatially varying relationships")
-    article2 = GgscholarParsing("Modeling spatial dimensions of housing prices in Milwaukee, WI")
-    print(article1)
-    print(article2)
-    
-    ### Saving the results in a graph DB`
+    ### Saving the results in a graph DB
